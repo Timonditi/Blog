bffde6f5677b8c447170741920826394c92f5f36
odongo <todongoonditi@gmail.com>
2023-12-08 01:32:11 +0300

    setup completed

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter, Routes, Route } from "react-router-dom" 
+import './App.css'
+import Home from "./pages/Home"
+import Layout from "./Layout/Layout"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Addpost from "./pages/Addpost"
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}/>
+        <Route path="Login" element={<Login />} />
+        <Route path="Register" element={<Register />} />
+        <Route path="Addpost" element={<Addpost />}/>
+        </Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/Updatepassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Updatepassword() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
