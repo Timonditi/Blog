329b5bb23ad3aa208a5d3edf944457f8516e172a
odongo <todongoonditi@gmail.com>
2023-12-07 18:05:15 +0300

    initial crud code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+    def index
+    end
+    
+    def show
+    end
+
+    def new
+    end    
+    
+    def create
+    end
+    
+    def update
+    end
+
+    def edit
+    end    
+    
+    def destroy
+    end    
+
+    private
+
+    def post_params
+    end    
 end
