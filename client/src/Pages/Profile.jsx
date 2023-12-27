4e683d5afa40b91c0b771f4506e259ac5d7549c9
Tim <todongoonditi@gmail.com>
2023-12-28 01:51:53 +0300

    done

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -5,23 +5,19 @@ export default function Profile() {
   const { current_user, logout } = useContext(AuthContext)
   return (
     <div>
-      <h2>
+      <h2 className='font-serif'>
         Hello <span>{current_user && current_user.name}</span>
       </h2>
-      <i></i>
-      <h3>Information</h3>
+
       <hr />
-      <div>
-        <img className="w-8 h-8 rounded-full" src={current_user ? current_user.profile_image : ""} lt="user photo"/>
+      <div className=' pt-2'>
+        <img className="w-40 h-40 " src={current_user ? current_user.profile_image : ""} lt="user photo"/>
       </div>
-      <div>
+      <div className='font-serif pt-2'>
         <h6>{current_user && current_user.username}</h6>
       </div>
-      <div>
+      <div className='font-serif pt-2 pb-4'>
         <h6>{current_user && current_user.email}</h6>
-      </div>
-      <div>
-        <h6>{current_user && current_user.created_at}</h6>
       </div>
       <button className='border-2 border-orange-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-orange-500 hover:text-white' onClick={() => logout()}>Logout</button>
     </div>
